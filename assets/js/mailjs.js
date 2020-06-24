b053a7ca840e5907f3ce36ace7b36e4c283a7e88
pyleks <aleksander.andersen@outlook.ie>
2020-06-24 21:16:01 +0100

    Added mail JS properly

--- assets/js/mailjs.js
+++ assets/js/mailjs.js
@@ -1,34 +1,33 @@
 function sendMail(contactForm) {
-    // Free mail API with restrictions, aquired from https://www.mailjs.com
-    emailjs.init("user_qeOnqYBPzVybZ4G2BOqsg");
+    // Free mail API with restrictions, obtained from https://www.mailjs.com
+    emailjs.init("user_3G60vDRXrJTJeNKzSxOOV");
     let templateParams = {
-        "first_name": contactForm.name.value,
+        "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "Blockworm_feedback": contactForm.feedback.value
+        "portfolio_message": contactForm.feedback.value
     };
     // Sends the email through mailJS
     // Also makes the button responsive when clicked, if success the button will changed text
     // And become green
-    emailjs.send("gmail", "blockworm_feedback", templateParams)
+    emailjs.send("gmail", "portfolio", templateParams)
         .then(
-            function(response) {
-                if(response) {
-                    $("#submit-button").text("Thank you");
-                    $("#submit-button").removeClass("btn-secondary");
-                    $("#submit-button").addClass("bg-green");
-                    $("#submit-button").addClass("font_white");
+            function (response) {
+                if (response) {
+                    $("#submit-button").text("Thank you").removeClass("btn-secondary").addClass("bg-green font_white");
+
                 }
             },
             // Change the color of button to red, with "Try Again Later"
-            function(error) {
-                if(error) {
-                    $("#submit-button").text("Try Again Later");
-                    $("#submit-button").removeClass("btn-secondary");
-                    $("#submit-button").addClass("white_cl");
-                    $("#submit-button").addClass("red_bg");
+            function (error) {
+                if (error) {
+                    $("#submit-button").text("Try Again Later").removeClass("btn-secondary").addClass("white_cl red_bd");
+
                 }
             }
         );
     return false;  // To block from loading a new page
+
+
+
 
 }
