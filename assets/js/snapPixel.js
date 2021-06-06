29c973eb340b9244a8aceb1a56bddaeb27298127
Pyleks <aleksander.andersen@outlook.ie>
2021-06-06 15:10:14 +0100

    Removed old profile/web pictures and added new ones

--- assets/js/snapPixel.js
+++ assets/js/snapPixel.js
@@ -1,15 +1,26 @@
-(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
-{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
-a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
-r.src=n;var u=t.getElementsByTagName(s)[0];
-u.parentNode.insertBefore(r,u);})(window,document,
-    'https://sc-static.net/scevent.min.js');
+window.onload = function() {
+    (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+    {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+    a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+    r.src=n;var u=t.getElementsByTagName(s)[0];
+    u.parentNode.insertBefore(r,u);})(window,document,
+        'https://sc-static.net/scevent.min.js');
 
-snaptr('init', '3b5a3a63-36ab-49fe-9ed9-1b8965fbe750', {
-    'user_email': '__INSERT_USER_EMAIL__'
-});
+    let signup = document.getElementById("submit-button");
 
-snaptr('track', 'PAGE_VIEW');
-snaptr('track', 'SIGN_UP');
+    if (signup) {
+        (signup).addEventListener('click', pSubmit)
+    }
+
+    function pSubmit() {
+        snaptr('init', '3b5a3a63-36ab-49fe-9ed9-1b8965fbe750', {
+            'user_email': document.getElementById("email_id").value
+
+        });
+        snaptr('track', 'RESERVE');
+        console.log(document.getElementById("email_id").value)
+    }
+
+};
 
 // <!-- End Snap Pixel Code -->
